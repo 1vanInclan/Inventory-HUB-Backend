b3771da5633c506e3e310639b1f0259869e63c5d
Ivan <ivan.inclan.327b@gmail.com>
2026-02-24 19:17:20 -0600

    Controller generalizado

--- src/controllers/inventoryController.ts
+++ src/controllers/inventoryController.ts
@@ -1,81 +1,68 @@
-import productsData from '../data/products.json' with {type: 'json'}
-import categoriesData from '../data/categories.json' with {type: 'json'}
-import transactionsData from '../data/transactions.json' with {type: 'json'}
-
-import type { Product, Categorie, Transaction } from '../interfaces/inventory.interface.ts'
-
 import type { Request, Response } from 'express'
+import { InventoryModel } from '../models/localFileSystem/inventory.ts'
 
 class InventoryController {
 
-  private products: Product[]
-  private categories: Categorie[]
-  private transactions: Transaction[]
+  private inventoryModel: InventoryModel;
 
   constructor () {
-    this.products = productsData
-    this.categories = categoriesData
-    this.transactions = transactionsData
+    this.inventoryModel = new InventoryModel()
   }
 
   addTransaction = async (req: Request, res: Response) => {
   const { productId, type, quantity } = req.body
 
-  const transaccion = {
-    productId: Number(productId),
-    type: type,
-    quantity: Number(quantity)
+  try {
+    const transaction = await this.inventoryModel.addTransaction(productId, type, quantity)
+    res.json(transaction)
+  } catch(e) {
+    res.status(400).json({
+      success: false,
+      error: e instanceof Error ? e.message : 'Error desconocido'
+    })
+    }  
   }
-
-  const indexProduct = this.products.findIndex( product => product.id === Number(productId) )
-
-  if(type === 'IN'){
-    this.products[indexProduct].stock = this.products[indexProduct].stock + Number(quantity)
-  }
-
-  if(type === 'OUT'){
-    
-    this.products[indexProduct].stock = this.products[indexProduct].stock - Number(quantity)
-  }
-
-  this.transactions.push(transaccion)
-
-  return res.status(201).json({ message: "Transaccion creada" })
-  }
+  
 
   getInventoryValue = async (req: Request, res: Response) => {
 
-  const arrayWithDiscount = this.products.map(product => {
-
-    const { discount } = this.categories.find( category => category.id === product.categoryId )
-
-    return {
-      ...product,
-      priceWithDiscount: product.price * (1 - discount/100)
+    try {
+      const inventoryValue = await this.inventoryModel.getInventoryValue()
+      res.json({InventoryValue: inventoryValue})
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        error: e instanceof Error ? e.message : 'Error desconocido'
+      })
     }
-  })
-
-  const result = arrayWithDiscount.reduce((acc, product) => {
-    acc = acc + (product.priceWithDiscount * product.stock)
-    return acc
-  }, 0)
-
-  return res.status(200).json({ ValorTotal: result })
-  
   }
 
   getTransactionsById = async (req: Request, res: Response) => {
-  const { productId } = req.params
-
-  const findTransactions = this.transactions.filter( transaction => transaction.productId === Number(productId))
-
-  res.status(200).json(findTransactions)
+    const { productId } = req.params
+    try {
+      const results = await this.inventoryModel.getTransactionById(Number(productId))
+      res.status(200).json(results)
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        error: e instanceof Error ? e.message : 'Error desconocido'
+      })
+    }
 
   }
 
   getProductsLowStock = async (req: Request, res: Response) => {
-  const result = this.products.filter( product => product.stock < 5)
-  res.status(200).json(result)
+    
+    try {
+      const products = await this.inventoryModel.getProductsLowStock()
+      res.status(200).json(products)
+    }catch (e) {
+      res.status(400).json({
+        success: false,
+        error: e instanceof Error ? e.message : 'Error desconocido'
+      })
+    }
+    
   }
 
 
---
+++ src/models/localFileSystem/inventory.ts
@@ -0,0 +1,71 @@
+import productsData from '../../data/products.json' with {type: 'json'}
+import categoriesData from '../../data/categories.json' with {type: 'json'}
+import transactionsData from '../../data/transactions.json' with {type: 'json'}
+
+import type { Product, Categorie, Transaction } from '../../interfaces/inventory.interface.ts'
+
+
+  const products: Product[] = productsData
+  const categories: Categorie[] = categoriesData
+  const transactions: Transaction[] = transactionsData
+
+export class InventoryModel {
+
+  async addTransaction(productId: number, type: 'IN' | 'OUT', quantity: number) {
+    
+    const transaccion = {
+      productId: Number(productId),
+      type: type,
+      quantity: Number(quantity)
+    }
+
+    const indexProduct = products.findIndex( product => product.id === Number(productId) )
+
+    if(type === 'IN'){
+      products[indexProduct].stock = products[indexProduct].stock + Number(quantity)
+    }
+
+    if(type === 'OUT'){
+      
+      products[indexProduct].stock = products[indexProduct].stock - Number(quantity)
+    }
+
+    transactions.push(transaccion)
+
+    return transaccion
+  }
+
+  async getInventoryValue() {
+    const arrayWithDiscount = products.map(product => {
+    
+        const category = categories.find(cat => cat.id === product.categoryId)!;
+        const { discount } = category;
+    
+        return {
+          ...product,
+          priceWithDiscount: product.price * (1 - discount/100)
+        }
+      })
+    
+      const result = arrayWithDiscount.reduce((acc, product) => {
+        acc = acc + (product.priceWithDiscount * product.stock)
+        return acc
+      }, 0)
+    
+      return result       
+  }
+
+  async getTransactionById(productId: number) {
+
+    const findTransactions = transactions.filter( transaction => transaction.productId === productId)
+
+    return findTransactions
+
+  }
+
+  async getProductsLowStock() {
+    const result = products.filter( product => product.stock < 5)
+    return result
+  }
+
+}
