b7131469be7e9990cd482e59b2cfada26fd71f30
Ivan <ivan.inclan.327b@gmail.com>
2026-03-02 16:20:03 -0600

    Add documentation swagger

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ dotenv.config()
 import express from 'express'
 import cors from 'cors'
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
 import { createTransactionsRouter } from './src/routes/transactions.ts'
 import { createStatsRouter } from './src/routes/stats.ts'
 import { createProductsRouter } from './src/routes/products.ts'
@@ -17,6 +20,33 @@ app.use(express.json())
 app.use(cors())
 
 connectDB();
+
+// 1. Configuración de Metadatos
+const swaggerOptions: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'The Inventory Hub API',
+      version: '1.0.0',
+      description: 'API para la gestión de inventarios - Fullstack Project',
+      contact: {
+        name: 'Ivan',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor de Desarrollo (Docker)',
+      },
+    ],
+  },
+  // 2. Ruta a los archivos que contienen las anotaciones
+  apis: ['./server.ts', './src/routes/*.ts'], 
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/auth', createAuthRouter())
  
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,7 +9,98 @@ export const createAuthRouter = () => {
 
   const authRouter = Router()
 
+  /**
+ * @openapi
+ * /auth/register:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    summary: Registrar usuario
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                example: ivan
+ *              email:
+ *                type: string
+ *                example: iva@gmail.com
+ *              password:
+ *                type: string
+ *                example: 123456
+ *              role:
+ *                type: string
+ *                example: admin
+ *    responses:
+ *      200:
+ *        description: Registro exitoso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Usuario registrado con exito
+ *                data:
+ *                  type: object
+ *      400:
+ *        description: Credenciales inválidas
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Datos Invalidos
+ *                errors:
+ *                  type: object
+ */
+
   authRouter.post('/register', userController.register)
+
+
+/**
+ * @openapi
+ * /auth/login:
+ *  post:
+ *    tags:
+ *      - Auth
+ *    summary: Iniciar sesión de usuario
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                example: ivan
+ *              password:
+ *                type: string
+ *                example: 123456
+ *    responses:
+ *      200:
+ *        description: Login exitoso, retorna el token
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Loggeado con exito
+ *                token:
+ *                  type: string
+ *      401:
+ *        description: Credenciales inválidas
+ */
 
   authRouter.post('/login', userController.login)
 
